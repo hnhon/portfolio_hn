f11bcf235fc4f39a5e8a55854f7923ede77436e0
hhon <>
2020-04-28 09:39:54 -0700

    Updates

---
+++ precache-manifest.3d2d99ded1bf774dc1eaa7b16e7721bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22e25c89fdccd4c0c5da35cc19aa6e49",
+    "url": "/portfolio_hn/index.html"
+  },
+  {
+    "revision": "6cce011926484c8ff181",
+    "url": "/portfolio_hn/static/css/main.909615ab.chunk.css"
+  },
+  {
+    "revision": "c141181f2b17eb63db8a",
+    "url": "/portfolio_hn/static/js/2.3fbb97d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio_hn/static/js/2.3fbb97d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cce011926484c8ff181",
+    "url": "/portfolio_hn/static/js/main.61911909.chunk.js"
+  },
+  {
+    "revision": "648660a67750e82c7e71",
+    "url": "/portfolio_hn/static/js/runtime-main.d0d19afc.js"
+  }
+]);
