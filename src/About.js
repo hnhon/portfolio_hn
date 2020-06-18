a56fa43bbdd2cfbb3d51686c6eeb6f80131b3b3b
hhon <alexhnhon@gmail.com>
2020-06-18 11:40:19 -0700

    a tag scroll

--- src/About.js
+++ src/About.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 function About() {
     return (
         <div id='about' className='banner'>
             <h2>Hiunam Hon</h2>
-            <p>Hi, I am a self-taught web developer. I learn by building stuffs and enjoy learning new skills from differnt projects. Feel free to <a href='#contact'>get in touch</a>.</p>
+            <p>Hi, I am a self-taught web developer. I learn by building stuffs and enjoy learning new skills from differnt projects. Feel free to <AnchorLink href='#contact'>get in touch</AnchorLink>.</p>
         </div>
     )
 };
